7c288cf97c99f22c983adeb5fccb6f5f4b09da61
Eon Viljoen <eonviljoen.ev@gmail.com>
2020-01-22 21:03:36 +0200

    Image scrapper

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
-from lxml import html
+# from lxml import html
 
-# # Search
-# search = input("Enter Search Term:")
-# params = {"q": search}
-# link = "https://www.bing.com/search?q=pizza"
-# r = requests.get(link)
-#
-# soup = BeautifulSoup(r.content, "html.parser")
-# # Find Results with id being b_results
-# results = soup.find("ol", {"id": "b_results"})
-# links = results.findAll("li", {"class": "b_algo"})
-# # doesn't print out now
-# # print(r.text)
-# # print(results)
-# # print(links)
-#
-# for item in links:
-#     item_text = item.find("a").text
-#     item_href = item.find("a").attrs["href"]
-#
-#     if item_text and item_href:
-#         print(item_text)
-#         print(item_href)
-#
+# Search
+search = input("Enter Search Term:")
+params = {"q": search}
+link = "https://www.bing.com/search?q=pizza"
+r = requests.get("http://www.bing.com/search", params=params)
+
+soup = BeautifulSoup(r.content, "html.parser")
+# Find Results with id being b_results
+results = soup.find("ol", {"id": "b_results"})
+links = results.findAll("li", {"class": "b_algo"})
+# doesn't print out now
+# print(r.text)
+# print(results)
+# print(links)
+
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
+        # print("Parent:", item.find("a").parent.parent.find("p").text)
+        children = item.find("h2")
+
+        print("Next Sibling of h2:", children.next_sibling)
+
+
 # print(results)
 
-URL = "https://www.google.com/search?&q=python+sessions+is+not+callable"
-session_requests = requests.session()
-result = session_requests.get(URL, headers = dict(referer = URL))
-tree = html.fromstring(result.content)
-units = tree.xpath('//text()')
-print(units)
+
+
+
+# URL = "https://www.google.com/search?&q=python+sessions+is+not+callable"
+# session_requests = requests.session()
+# result = session_requests.get(URL, headers = dict(referer = URL))
+# tree = html.fromstring(result.content)
+# units = tree.xpath('//text()')
+# print(units)
